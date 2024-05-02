87a894ce5f4d1bcb9cb1f4b1ccf0722695b4febc
Lance Desk <lncedesk@gmail.com>
2024-05-02 08:46:07 -0700

    Use mypy to validate the following piece of code and apply any necessary changes.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -10,13 +10,13 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zoom in on the array by repeating each element by a given factor
     """
-    zoomed_in: List = [
+    zoomed_in = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 zoom_2x = zoom_array(array)
 zoom_3x = zoom_array(array, 3.0)
