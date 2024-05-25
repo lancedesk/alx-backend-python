342c0863f4d2cb80365700d77c904a2768a64a58
Lance Desk <lncedesk@gmail.com>
2024-05-25 06:20:29 -0700

    Implement TestAccessNestedMap.test_access_nested_map_exception. Use the assertRaises context manager to test that a KeyError is raised for the following inputs (use @parameterized.expand):

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,6 +29,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test access_nested_map function raises KeyError for invalid paths.
+
+        Args:
+            nested_map (dict): The nested map.
+            path (tuple): The path of keys.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), repr(path[-1]))
+
 
 if __name__ == '__main__':
     unittest.main()
